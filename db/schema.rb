3ca01304516cf1ad1909bebba0a087a79fb4493d
narumihamasaki <ojtmkforever@gmail.com>
2017-04-21 01:21:14 +0000

    validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420045915) do
+ActiveRecord::Schema.define(version: 20170421002909) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "Status",     limit: 1
   end
 
 end
